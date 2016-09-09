5a244a70ee6cba1222d18bc1b8e33daf3938baef
Jack Vine <jackv949@gmail.com>
2016-09-09 17:13:31 +0930

    Camera follows ahead of player based on X input.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Follow Target")]
     //The target to follow
     [Tooltip("The target to follow.\nWill find a GameObject tagged 'Player' if left blank")]
     public Transform target;
@@ -13,9 +15,20 @@ public class CameraFollow : MonoBehaviour
     public Vector2 offset;
 
     //For lerping between positions
+    [Space()]
     [Range(0f, 1f)]
     [Tooltip("How closely the camera follows the target.\n1 follows perfectly, less than one is smooth.")]
     public float smoothing = 0.5f;
+
+    [Header("Follow Ahead")]
+    public bool followAhead = true;
+    public float distanceAhead = 5f;
+    [Range(0, 1f)]
+    public float distanceSmoothing = 0.1f;
+    private float targetDistance;
+
+    private InputDevice device;
+    private float inputX;
 
     //Desired camera position (for lerping)
     private Vector3 targetPosition;
@@ -36,6 +49,14 @@ public class CameraFollow : MonoBehaviour
             Debug.Log("Camera has no target!");
     }
 
+    void Update()
+    {
+        device = InputManager.ActiveDevice;
+
+        //Get X axis input
+        inputX = Mathf.Clamp(device.DPadX + device.LeftStickX, -1f, 1f);
+    }
+
     void LateUpdate()
     {
         //Make sure there is a target to follow
@@ -44,6 +65,13 @@ public class CameraFollow : MonoBehaviour
             //Set position with offset
             targetPosition.x = target.position.x + offset.x;
             targetPosition.y = target.position.y + offset.y;
+
+            if (followAhead)
+            {
+                //Follow ahead of target based on X input
+                targetDistance = Mathf.Lerp(targetDistance, inputX * distanceAhead, distanceSmoothing);
+                targetPosition.x += targetDistance;
+            }
 
             //Lerp camera position
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
