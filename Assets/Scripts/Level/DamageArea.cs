47511bea78d159437fdef288f57ea6c2a6ae50bb
Jack Vine <jackv949@gmail.com>
2016-09-12 16:51:52 +0930

    Started basic Player health and damage area (below level).

---
+++ Assets/Scripts/Level/DamageArea.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageArea : MonoBehaviour
+{
+    public int damage = 100;
+
+    public enum DamageType { OnEnter, PerSecond }
+    public DamageType damageType = DamageType.OnEnter;
+
+    public string targetTag = "Player";
+
+    private PlayerStats stats = null;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == targetTag)
+        {
+            if (stats != other.GetComponent<PlayerStats>())
+            {
+                stats = other.GetComponent<PlayerStats>();
+
+                switch (damageType)
+                {
+                    case DamageType.OnEnter:
+                        stats.RemoveHealth(damage);
+                        break;
+                    case DamageType.PerSecond:
+                        StartCoroutine("DrainHealth");
+                        break;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == targetTag)
+            stats = null;
+    }
+
+    IEnumerator DrainHealth()
+    {
+        while (stats)
+        {
+            stats.RemoveHealth(damage);
+
+            if (stats.currentHealth <= 0)
+                break;
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int currentHealth = 100;
+    public int maxHealth = 100;
+
+    public void RemoveHealth(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
